774f1f898bb05c06725ca8943ea8a9f63dc14ccd
abhayminer01 <abhayvapvl@gmail.com>
2025-06-16 20:41:44 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path='*' element={ <NoPage /> } />
+        <Route path='/' element={ <Home /> } />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
